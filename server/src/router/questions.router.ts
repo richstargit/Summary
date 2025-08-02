478e7dd48636cebfd0640fd062f40482eeef88ef
Tanabordee Pateepbutsagon <102315593+Tanabordeee@users.noreply.github.com>
2025-08-03 01:28:37 +0700

    feat : add function Create Question

--- server/src/router/questions.router.ts
+++ server/src/router/questions.router.ts
@@ -1,6 +1,7 @@
-import { FindQuestion } from '@/service/question.service';
+import { CreateQuestion, FindQuestion } from '@/service/question.service';
 import { Elysia } from 'elysia';
 
 
-export const QuestionsRoute = new Elysia({})
-    .get('question/:id', FindQuestion);
+export const QuestionsRoute = new Elysia({prefix:"/api"})
+    .get('question/:id', FindQuestion)
+    .post("question" , CreateQuestion)
