3ca8362c743eeefd3b03eb42c65459ae7afc4f7e
Unknowkubbrother <natchnon654321@gmail.com>
2025-08-03 00:35:45 +0700

    update register without encrpt

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,30 +2,41 @@ import { Elysia } from "elysia";
 import { cors } from "@elysiajs/cors";
 import "@/config/db.setup";
 import { logger } from "@chneau/elysia-logger";
-import { swagger } from '@elysiajs/swagger'
-import { TemplateRoute } from "@/router/template.router"
+import { swagger } from "@elysiajs/swagger";
+import { jwt } from "@elysiajs/jwt";
 
+import { TemplateRoute } from "@/router/template.router";
+import { AccountRoute } from "@/router/accounts.router";
 /**
-* @comment Create a new Elysia app
-*/
+ * @comment Create a new Elysia app
+ */
 const app = new Elysia()
-  .use(swagger({
-    path: '/api/docs',
-    documentation: {
-      info: {
-          title: 'Summary Documentation',
-          version: '1.0.0'
-      }
-    }
-  }))
+  .use(
+    jwt({
+      name: "jwt",
+      secret: process.env.SECRET_JWT || 'hex',
+    })
+  )
+  .use(
+    swagger({
+      path: "/api/docs",
+      documentation: {
+        info: {
+          title: "Summary Documentation",
+          version: "1.0.0",
+        },
+      },
+    })
+  )
   .use(logger())
   .use(cors())
   .use(TemplateRoute)
-  .get("/", () =>{
-    return {message: "Hello, Elysia! by HEX CODE"};
+  .use(AccountRoute)
+  .get("/", () => {
+    return { message: "Hello, Elysia! by HEX CODE" };
   })
   .listen(process.env.SERVER_PORT || 3001);
 
 console.log(
-    `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
