b7b8c7b0c738c2332a30ebcf7f8fdf06734c8864
unknown <siwakchuhirun2004@gmail.com>
2025-08-03 12:44:27 +0700

    question api

--- client/app/questions/[id]/page.tsx
+++ client/app/questions/[id]/page.tsx
@@ -22,7 +22,7 @@ export default function QuestionPage() {
 
     const [showAnswer,setShowAnswer] = useState(false)
     const [data, setData] = useState<Data>()
-
+    
     useEffect(() => {
     fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/question/${id}`)
       .then(res => res.json())
