68bda364c74125f3ce68b575674b756709c80779
unknown <siwakchuhirun2004@gmail.com>
2025-08-03 13:16:34 +0700

    summary

--- client/app/question/[id]/page.tsx
+++ client/app/question/[id]/page.tsx
@@ -12,40 +12,41 @@ type Question = {
 }
 
 type Data = {
-    title: string,
-    data : Question[]
+  title: string,
+  data: Question[]
 }
 
 export default function QuestionPage() {
-    const params = useParams<{ id: string }>()
-    const id_question = params?.id ?? ''
+  const params = useParams<{ id: string }>()
+  const id_question = params?.id ?? ''
 
-    const [showAnswer,setShowAnswer] = useState(false)
-    const [data, setData] = useState<Data>()
-    
-    useEffect(() => {
+  const [showAnswer, setShowAnswer] = useState(false)
+  const [data, setData] = useState<Data>()
+
+  useEffect(() => {
     fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/question/${id_question}`)
       .then(res => res.json())
       .then(json => {
-      if (json.status === 200) {
-        setData(json.body)
-      } else {
-        console.error("เกิดข้อผิดพลาด: status != 200", json)
-      }
-    }).catch(err => console.error("Fetch error:", err))
+        if (json.status === 200) {
+          setData(json.body)
+        } else {
+          console.error("เกิดข้อผิดพลาด: status != 200", json)
+        }
+      }).catch(err => console.error("Fetch error:", err))
   }, [])
 
-    return (
-        <div>
-            <h1 className="text-center text-3xl font-bold tracking-tight text-primary mt-8">
-  🧬 Question #{data?.title}
-</h1>
-            {data?.data.map((d,index)=>{
-                return <div key={index} className='mt-5 mb-5'><CardQuestion showAnswer={showAnswer} data={d}></CardQuestion></div>
-            })}
-            <Button className='mx-auto block mt-4 mb-35' onClick={()=>setShowAnswer(true)}>
-                Show Answer
-            </Button>
-        </div>
-    )
+  return (
+    <div>
+      <h1 className="text-center text-3xl font-bold tracking-tight text-primary mt-8">
+        🧬 Question #{data?.title}
+      </h1>
+      <Button className='mx-auto block mt-4 mb-5'>Show Summary</Button>
+      {data?.data.map((d, index) => {
+        return <div key={index} className='mt-5 mb-5'><CardQuestion showAnswer={showAnswer} data={d}></CardQuestion></div>
+      })}
+      <Button className='mx-auto block mt-4 mb-35' onClick={() => setShowAnswer(true)}>
+        Show Answer
+      </Button>
+    </div>
+  )
 }
